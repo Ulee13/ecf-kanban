6021d1f22e2a2a17d88addaea6729ec82770e207
Ulee13 <ulee13@gmail.com>
2025-04-16 00:56:38 +0200

    Add files via upload

--- src/dao/DaoKanban.php
+++ src/dao/DaoKanban.php
@@ -1,5 +1,8 @@
 <?php
 namespace kanban\dao;
+use kanban\dao\Database;
+use kanban\dao\Requetes;
+use kanban\metier\SupProjet;
 
 class DaoKanban {
     private \PDO $conn;
@@ -11,4 +14,20 @@ class DaoKanban {
      * Récupérer la liste des icones
      * @return array
      */
+
+    
+    
+// ***** Get ****** //
+
+public function getSupProjet() : array {
+    $cursor = $this->conn->query(Requetes::SUPPROJETS_GET);
+    $supProjets = array();
+    while ($row = $cursor->fetch(\PDO::FETCH_OBJ)) {
+        $supProjets[] = new SupProjet($row->id_modulex, $row->id_projet, $row->id_supprojet, $row->date_creation_supprojet, $row->coul_supprojet, $row->nom_module_projet, $row->id_icone);
+    }
+    return $supProjets;
+}
+
+
+
 }
--- src/dao/Requetes.php
+++ src/dao/Requetes.php
@@ -2,24 +2,29 @@
 namespace kanban\dao;
 
 class Requetes {
-    private \PDO $conn;
-    // je déclare le constructeur qui permet de regrouper les méthodes $conn et $requete
-    public function __construct() {
-        $this->conn = Database::getConnection();
-    }
+    // private \PDO $conn;
+    // // je déclare le constructeur qui permet de regrouper les méthodes $conn et $requete
+    // public function __construct() {
+    // $this->conn = Database::getConnection();
+    // }        
     
-    /**
-     * Récupérer la liste des icones
-     * @return array
-     */
-    public function getIcones(): array {
-        $sql = "SELECT id, libelle FROM icone";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->execute();
-        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-    }
-    /**
-     * Récupérer la liste des colonnes
-     * @return array
-     */
+    // /**
+    //  * Récupérer la liste des icones
+    //  * @return array
+    //  */
+    // public function getIcones(): array {
+    //     $sql = "SELECT id, libelle FROM icone";
+    //     $stmt = $this->conn->prepare($sql);
+    //     $stmt->execute();
+    //     return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    // }
+
+    // /**
+    //  * Récupérer la liste des colonnes
+    //  * @return array
+    //  */
+
+    //public const SUPPROJET_GET = "Select id_modulex, id_projet, id_supprojet, date_creation_supprojet, coul_supprojet, nom_module_projet, id_icone";
+    //public const SUPPROJET_GET_BY_ID = "Select categorie.idc, libellec, idp, libellep, prixp, compop, pathImgP from categorie JOIN plat ON categorie.idc=plat.idc where idp = :id";
+    public const SUPPROJETS_GET = "Select id_modulex, id_projet, id_supprojet, date_creation_supprojet, coul_supprojet, nom_module_projet, id_icone from supProjet order by id_supprojet";
 }
